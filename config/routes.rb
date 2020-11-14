7171cd4256a7d8146a8d9f4a5d45c4c37ac9b323
Miguel Guardia <guardia@ualberta.ca>
2020-11-13 16:08:26 -0800

    Complete Endpoint-Finder
    Create Search controller for Items and Merchants
    Create single and multi find methods for Item and Merchant models
    Create routes for finding endpoints
    Create Specs to test new methods, controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+
+      get "/items/find_all", to: 'items/search#index'
+      get "/items/find", to: 'items/search#show'
       resources :items do
         get :merchants, only: :show, to: 'items/merchants#index'
       end
 
+      get '/merchants/find', to: 'merchants/search#show'
+      get '/merchants/find_all', to: 'merchants/search#index'
       resources :merchants do
         get :items, only: :show, to:'merchants/items#index'
       end
+
 
 
     end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  decribe 'validations' do
+  describe 'validations' do
     it { should validate_presence_of :credit_card_number }
     it { should validate_presence_of :result }
   end
